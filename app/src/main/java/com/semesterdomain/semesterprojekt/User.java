b2b38f3f00d9459d3db86a656a15bc93e5e516bc
egoBrain92 <egobrain92@googlemail.com>
2016-06-26 02:48:27 +0200

    javadoc removed

--- app/src/main/java/com/semesterdomain/semesterprojekt/User.java
+++ app/src/main/java/com/semesterdomain/semesterprojekt/User.java
@@ -2,28 +2,56 @@ package com.semesterdomain.semesterprojekt;
 
 import java.io.Serializable;
 
+/**
+ * The type User.
+ */
 public class User implements Serializable {
 
     private int userId;
     private String username;
 
+    /**
+     * Instantiates a new User.
+     *
+     * @param username the username of the User
+     */
     public User(String username) {
         this.username = username;
         this.userId = 1;
     }
 
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets username.
+     *
+     * @param username the username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets user id.
+     *
+     * @return the user id
+     */
     public int getUserId() {
         return userId;
     }
 
+    /**
+     * Sets user id.
+     *
+     * @param userId the user id
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
